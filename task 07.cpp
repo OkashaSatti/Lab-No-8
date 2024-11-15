f279853454402ee6bccaaebfc82902a44bafdb5c
OkashaSatti <okashasatti30@gmail.com>
2024-11-15 22:43:23 +0500

    Create task 07.cpp

---
+++ task 07.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int month;
+    cout<<"Enter month [1-12]: ";
+    cin>>month;
+    switch(month)
+    {
+        case 1:
+        cout<<"It is Winter";
+        break;
+        case 2:
+        cout<<"It is Winter";
+        break;
+        case 3:
+        cout<<"It is Spring";
+        break;
+        case 4:
+        cout<<"It is Spring";
+        break;
+        case 5:
+        cout<<"It is Spring";
+        break;
+        case 6:
+        cout<<"It is Summer";
+        break;
+        case 7:
+        cout<<"It is Summer";
+        break;
+        case 8:
+        cout<<"It is Summer";
+        break;
+        case 9:
+        cout<<"It is Autumn";
+        break;
+        case 10:
+        cout<<"It is Autumn";
+        break;
+        case 11:
+        cout<<"It is Autumn";
+        break;
+        case 12:
+        cout<<"It is Winter";
+        break;
+        default:
+        cout<<"Invalid month";
+        break;
+    }
+}
